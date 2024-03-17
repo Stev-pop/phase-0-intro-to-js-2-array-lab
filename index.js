bf28f93820b7422aef57cbf28dbbd33ec15db7a2
Stephen Onyango <onyangosteve01@gmail.com>
2024-03-17 22:43:53 +0300

    adds functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){ 
+    const newCats = [...cats.slice(0), 'Broom']
+    return newCats;
+}
+function prependCat(){
+    const newCats =['Arnold', ...cats.slice()]
+    return newCats;
+}
+function removeLastCat(){
+    const newCats = [...cats.slice(0, -1)]
+    return newCats;
+}
+function removeFirstCat(){
+    const newCats = [...cats.slice(1)]
+    return newCats;
+}
